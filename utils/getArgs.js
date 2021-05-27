eb72672e1bc2a82e7649fd30ddf9d1f7a38a7e24
Michaël CARRETTE <miccarr@me.com>
2021-05-27 12:25:50 +0200

    Windows compatibility

--- utils/getArgs.js
+++ utils/getArgs.js
@@ -1,5 +1,9 @@
 module.exports = function() {
+  if (process.argv.includes('--args')) {
+    console.log(JSON.stringify(process.argv));
+    return process.exit(0);
+  }
   return process.argv.slice(
-    process.argv.findIndex(x => x.endsWith('/index.js') || x.endsWith('/ranchboy')) + 1
+    process.argv.findIndex(x => x.endsWith('/index.js') || x.endsWith('/ranchboy') || x.endsWith('/node.exe')) + 1
   );
 }
